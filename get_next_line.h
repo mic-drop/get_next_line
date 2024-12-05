d60899beea4ec8ac1ecbca7da0f4eeb91fbcc48f
mic-drop <mserra-p@c1r6s13.42lisboa.com>
2024-12-05 16:16:38 +0000

    removes shif_reset from library

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mserra-p <mserra-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 15:44:57 by mserra-p          #+#    #+#             */
-/*   Updated: 2024/12/05 16:01:20 by mserra-p         ###   ########.fr       */
+/*   Updated: 2024/12/05 16:16:21 by mserra-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int		ft_strlen(const char *src);
 char	*ft_strjoin(char *s1, char *s2);
 int		ft_check_nl(char *line);
 char	*ft_get_line(char *str);
-char	*ft_shift_reset(char *buffer, int nl_index, int buff_size);
 char	*ft_reset__and_return(char *buffer, char *str, int buff_size);
 char	*clean_exit(char *str);
 #endif
